6cd926265cfdfb11fb52de956673ac3b98a0d892
SMTDante <smtdante@gmail.com>
2017-07-10 12:08:41 -0400

    Kata Array

---
+++ mm_kata_array_function.rb
@@ -0,0 +1,33 @@
+def numbers()
+  (1..100).to_a
+  end
+
+def sequence()
+  numbers.collect do |x|
+    if (x % 15 == 0)
+      "Mined Minds"
+    elsif (x % 3 == 0)
+      "Mined"
+    elsif (x % 5 == 0)
+      "Minds"
+    end
+  end
+end
+
+
+# def a(a)
+ #   a = [*1..100]
+ #    if a[2] == 3
+  #    a[2] = "mined"
+ #   end
+ #   a
+ # end
+
+ #def a()
+ #  a = []
+# end
+# counter = 0
+# 100.times do
+#   counter += 1
+#   a << counter
+# end
---
+++ test_mm_kata_array_function.rb
@@ -0,0 +1,34 @@
+require "minitest/autorun"
+require_relative "mm_kata_array_function.rb"
+
+class TestMMKataArrayFunction < Minitest::Test
+  def test_1_equals_1
+    assert_equal(10-5, 3+2)
+  end
+  def test_count_100
+    results = numbers
+    assert_equal(100, results.count)
+    assert_equal(Array, results.class)
+  end
+  def test_first_number
+    assert_equal(1, numbers[0])
+  end
+  def test_third_number
+     assert_equal("Mined", sequence[2])
+   end
+   def test_thirty_third_number
+     assert_equal("Mined", sequence[32])
+   end
+   def test_fifth_number
+     assert_equal("Minds", sequence[4])
+   end
+   def test_fitieth_fifth_number
+     assert_equal("Minds", sequence[54])
+   end
+   def test_fifteenth_number
+     assert_equal("Mined Minds", sequence[14])
+   end
+   def test_nintieth_number
+     assert_equal("Mined Minds", sequence[89])
+   end
+end
